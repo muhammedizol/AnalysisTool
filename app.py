094d2a01f43b5a1953916534426b6b06754a0b63
muhammedizol <muhammedmelik_izol@hotmail.com>
2024-04-29 15:36:42 -0400

    Added correlation matrix

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.preprocessing import LabelEncoder
+
 
 def main():
     # Set the title of the Streamlit app
@@ -26,69 +30,103 @@ def main():
         features = st.multiselect('Select features', data.columns.tolist())
         if features:
             # Filter the DataFrame based on the selected features
-            result = data[features]
+            data = data[features]
             # Display the filtered DataFrame
-            st.dataframe(result)
+            st.dataframe(data)
+
+
+        # Allow the user to select a column
+        selected_column = st.selectbox('Select column to filter', data.columns.tolist())
+        if selected_column:
+            # Get the minimum and maximum values of the selected column
+            min_value = data[selected_column].min()
+            max_value = data[selected_column].max()
+
+            # Allow the user to select a range of values
+            values = st.slider('Select a range of values', min_value, max_value, (min_value, max_value))
+
+            # Filter the DataFrame based on the selected range
+            data = data[data[selected_column].between(values[0], values[1])]
 
         # Add aggregation functionality
         if st.checkbox('Aggregate Data'):
             st.subheader('Data Aggregation')
             agg_methods = ['sum', 'count', 'mean', 'min', 'max']
             selected_method = st.selectbox('Select aggregation method', agg_methods)
-            selected_feature = st.multiselect('Select feature to aggregate', result.columns.tolist())
-            groupby_columns = st.multiselect('Select columns to group by', result.columns.tolist())
+            selected_feature = st.multiselect('Select feature to aggregate', data.columns.tolist())
+            groupby_columns = st.multiselect('Select columns to group by', data.columns.tolist())
 
             # if st.button('Aggregate'):
             # Group the data by the selected columns and Aggregate the data based on the selected method
 
             if len(groupby_columns)>0 and len(selected_feature)>0:
                 if selected_method == 'sum':
-                    result = result.groupby(groupby_columns, as_index=False)[selected_feature].sum()
+                    data = data.groupby(groupby_columns, as_index=False)[selected_feature].sum()
                 elif selected_method == 'count':
-                    result = result.groupby(groupby_columns, as_index=False)[selected_feature].count()
+                    data = data.groupby(groupby_columns, as_index=False)[selected_feature].count()
                 elif selected_method == 'mean':
-                    result = result.groupby(groupby_columns, as_index=False)[selected_feature].mean()
+                    data = data.groupby(groupby_columns, as_index=False)[selected_feature].mean()
                 elif selected_method == 'min':
-                    result = result.groupby(groupby_columns, as_index=False)[selected_feature].min()
+                    data = data.groupby(groupby_columns, as_index=False)[selected_feature].min()
                 elif selected_method == 'max':
-                    result = result.groupby(groupby_columns, as_index=False)[selected_feature].max()
+                    data = data.groupby(groupby_columns, as_index=False)[selected_feature].max()
 
                 # Display the aggregated data
-                st.dataframe(result)
+                st.dataframe(data)
 
         # Add discretization functionality
 
         if st.checkbox('Discretize Data'):
             st.subheader('Data Discretization')
-            discretize_columns = st.multiselect('Select columns to discretize', result.columns.tolist())
+            discretize_columns = st.multiselect('Select columns to discretize', data.columns.tolist())
             for column in discretize_columns:
                 # Calculate the optimal number of bins
-                bins = calculate_optimal_bins(result[column])
+                bins = calculate_optimal_bins(data[column])
                 # Create labels for the bins
                 labels = range(1,bins+1)
                 # Discretize the data and add it as a new column
-                result[column+'_discretized'] = pd.cut(result[column], bins=bins, labels=labels)
+                data[column+'_discretized'] = pd.cut(data[column], bins=bins, labels=labels)
             # Display the DataFrame with the discretized data
-            st.dataframe(result)
+            st.dataframe(data)
+
+
+
+
+        # Correlation Matrix
+        le = LabelEncoder()
+        for column in data.select_dtypes(include=['object']).columns:
+            data[column] = le.fit_transform(data[column])
+
+        selected_columns = st.multiselect('Select columns for correlation matrix', data.columns.tolist())
+        if selected_columns:
+            # Filter the DataFrame based on the selected columns
+            filtered_data = data[selected_columns]
+
+            correlation_matrix = filtered_data.corr()
+            # Create a matplotlib figure
+            fig, ax = plt.subplots()
+            # Visualize the correlation matrix
+            sns.heatmap(correlation_matrix, annot=True, ax=ax)
+            st.pyplot(fig)
+
 
         # Add model building functionality
         if st.checkbox('Build Model'):
             st.subheader('Model Building')
             model_types = ['Classification', 'Regression']
             selected_model_type = st.selectbox('Select model type', model_types)
-            st.dataframe(result)
-            target_feature = st.selectbox('Select target feature', result.columns.tolist())
-            feature_columns = st.multiselect('Select feature columns',[col for col in result.columns.tolist() if col != target_feature])
+            st.dataframe(data)
+            target_feature = st.selectbox('Select target feature', data.columns.tolist())
+            feature_columns = st.multiselect('Select feature columns',[col for col in data.columns.tolist() if col != target_feature])
 
             # Split the data into training and testing sets
             X_train, X_test, y_train, y_test = train_test_split(
-                result[feature_columns], result[target_feature], test_size=0.2, random_state=42)
+                data[feature_columns], data[target_feature], test_size=0.2, random_state=42)
 
             if selected_model_type == 'Classification':
                 # Train a Logistic Regression model
                 model = LogisticRegression()
-                summary = model.fit(X_train, y_train)
-                summary.summary()
+                model.fit(X_train, y_train)
                 # Make predictions on the testing set
                 predictions = model.predict(X_test)
                 # Calculate the accuracy of the model
@@ -103,6 +141,7 @@ def main():
                 # Calculate the mean squared error of the model
                 mse = mean_squared_error(y_test, predictions)
                 st.write(f'Mean Squared Error: {mse}')
+
 
 
 def calculate_optimal_bins(data):
